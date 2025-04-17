035db7fa56014c50cf0eaaca1086e5d27bf0e7c4
kulkarnisanika <kulkarnisanika2@gmail.com>
2025-04-17 16:24:07 +0530

    final deployment after changing directory name

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,17 +8,16 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSLU1S26CMumrNoWFChDQLsxHsAhWV7qM",
-  authDomain: "netflix-gpt-314ec.firebaseapp.com",
-  projectId: "netflix-gpt-314ec",
-  storageBucket: "netflix-gpt-314ec.firebasestorage.app",
-  messagingSenderId: "792997702866",
-  appId: "1:792997702866:web:3360e62f3aa9fd9332910b",
-  measurementId: "G-6LPBYJ18V8"
+  apiKey: "AIzaSyAtr-YLF2WSFRf92iNc8czpF3YsFz1Hf9U",
+  authDomain: "cine-gpt-app.firebaseapp.com",
+  projectId: "cine-gpt-app",
+  storageBucket: "cine-gpt-app.firebasestorage.app",
+  messagingSenderId: "705401655903",
+  appId: "1:705401655903:web:024bf4a6462fa95e9241ea",
+  measurementId: "G-MVEVK74VJ4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth()
-;
